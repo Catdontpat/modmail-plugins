a688015b94a59e00f41f3cc2ad70c1c673374205
Cattu(Justin) <KirbyXstar@gmail.com>
2024-03-18 04:44:56 -0500

    initial commit

---
+++ voice/voice.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+
+class Voice(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_thread_ready(self, thread, creator, category, initial_message):
+        msg = thread.genesis_message
+        try:
+            channel = creator.voice.channel
+            if channel:
+                embed = discord.Embed(
+                    description=f'<@{creator.id}> is currently in <#{channel.id}>')
+                thread.send(embed=embed)
+            else:
+                return
+        except AttributeError:
+            return await thread.send('An error has occurred.')
