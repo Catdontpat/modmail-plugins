db2c7798ccc6b3cbd4110735ed3ce2f701521129
Cattu(Justin) <KirbyXstar@gmail.com>
2024-03-18 06:31:33 -0500

    updated voice

--- voice/voice.py
+++ voice/voice.py
@@ -6,17 +6,14 @@ class Voice(commands.Cog):
 
     @commands.Cog.listener()
     async def on_thread_ready(self, thread, creator, category, initial_message):
+        gmember: discord.Member = self.bot.get_guild(self.bot.guild_id).get_member(thread.recipient.id)
         msg = thread.genesis_message
-        try:
-            channel = creator.voice.channel
-            if channel:
-                embed = discord.Embed(
-                    description=f'<@{creator.id}> is currently in <#{channel.id}>')
-                thread.send(embed=embed)
-            else:
-                return
-        except AttributeError:
-            return await thread.send('An error has occurred.')
+        if gmember and gmember.voice:
+        embed = discord.Embed(
+            description=f'This User is currently in Voice Channel {gmember.voice.channel.mention}.',
+            color=discord.Color.blue()
+        )
+        await thread.channel.send(embed=embed)
         
 async def setup(bot):
     await bot.add_cog(Voice(bot))
